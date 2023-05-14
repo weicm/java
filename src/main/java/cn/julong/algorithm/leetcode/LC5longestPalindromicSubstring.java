ea4bed9b7431b07f545071195bebc09b08b1c12f
weichangming <weichangming@hualala.com>
2023-05-14 17:33:50 +0800

    LC5最长回文子串：隔板法

--- src/main/java/cn/julong/algorithm/leetcode/LC5longestPalindromicSubstring.java
+++ src/main/java/cn/julong/algorithm/leetcode/LC5longestPalindromicSubstring.java
@@ -5,77 +5,47 @@ package cn.julong.algorithm.leetcode;
  */
 public class LC5longestPalindromicSubstring {
     public static void main(String[] args) {
-        String s = "aacabdkacaa";
+        String s = "babad";
         // String s = "babad";
         // String s = "cbbd";
         // String s = "a";
 
-        System.out.println(longestPalindromewithDynamicPlannning(s));
+        System.out.println(longestPalindrome(s));
     }
 
-    /**
-     * 动态规划解法
-     *
-     * @param s
-     * @return
-     */
-    public static String longestPalindromewithDynamicPlannning(String s) {
-        if (null == s || s.length() < 2) {
-            return s;
-        }
-        int start = 0, end = 0, len = s.length();
-        boolean[][] state = new boolean[len][len];
-        for (int j = 1; j < s.length(); j++) {
-            for (int i = 0; i < j; i++) {
-                if (s.charAt(i) != s.charAt(j)) {
-                    state[i][j] = !false;
-                } else {
-                    if (j - i < 3) {
-                        state[i][j] = !true;
-                    } else {
-                        state[i][j] = state[i + 1][j - 1];
-                    }
-                }
-                if (!state[i][j] && j - i > end - start) {
-                    start = i;
-                    end = j;
-                }
+    static String longestPalindrome(String s) {
+        int maxLen = 0, start = 0, end = 0;
+        char[] ss = fenceStr(s, '#');
+        for(int i = 0; i < ss.length; i++) {
+
+            int len = (expandCenter(ss, i, i) - 1) / 2;
+
+            if(len > maxLen) {
+                maxLen = len;
+                int realIndex = (i - 1) / 2;
+                start = realIndex - (len - 1) / 2;
+                end = realIndex + len / 2;
             }
         }
-
         return s.substring(start, end + 1);
     }
-    /**
-     * 中心扩展法
-     * @param s
-     * @return
-     */
-    public static String longestPalindromewithMidExtend(String s) {
-        if (null == s && s.length() == 0) {
-            return "";
+
+    static char[] fenceStr(String s, char sep) {
+        int n = s.length();
+        char[] ss = new char[2 * n + 1];
+        for(int i = 0; i < ss.length; i++) {
+            ss[i] = i % 2 == 0 ? sep : s.charAt((i - 1) / 2);
         }
-        int start = 0, maxLen = 0;
-        for (int i = 0; i < s.length(); i++) {
-            // 判断奇数回文长度
-            int len1 = palindromeLen(s, i, i);
-            // 判断偶数回文长度
-            int len2 = palindromeLen(s, i, i + 1);
-            // 比较最长回文长难度，并修正start、maxLen
-            int len = Math.max(len1, len2);
-            if (len > maxLen) {
-                start = i - (len - 1) / 2;
-                maxLen = len;
-            }
+        return ss;
+    }
+
+    static int expandCenter(char[] ss, int l,  int r) {
+        int n = ss.length;
+        while(l >= 0 && r < n && ss[l] == ss[r]) {
+            l--;
+            r++;
         }
 
-        return s.substring(start, start + maxLen);
-    }
-    private static int palindromeLen(String s, int start, int end) {
-        for (; start >= 0 && end < s.length() && s.charAt(start) == s.charAt(end); ) {
-            start--;
-            end++;
-        }
-        int len = end - start - 1;
-        return len > 0 ? len : 0;
+        return r - l - 1;
     }
 }
